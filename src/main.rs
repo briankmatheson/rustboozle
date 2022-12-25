9f71ddf47afde21d7a972c76203b6968e9586ded
Brian Matheson <bmath@brianmatheson.com>
2022-12-25 10:13:27 -0500

    moar cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use nokhwa::{Camera, CameraFormat, FrameFormat};
+use nokhwa::pixel_format::RgbFormat;
+use nokhwa::{
+    Camera,
+    utils::{
+	RequestedFormatType,
+	RequestedFormat,
+	CameraIndex,
+    }
+};
+use mozjpeg::Compress;
+
 use std::fs::File;
 use std::io::Write;
 
+
 fn main() {
     let mut camera = Camera::new(
-	0,
-	Some(CameraFormat::new_from(640, 480, FrameFormat::MJPEG, 30)),
-    )
-    .unwrap();
+	CameraIndex::Index(0),
+	RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestResolution)
+    ).unwrap();
     camera.open_stream().unwrap();
-    let frame = camera.frame().unwrap();
+    let frame_buffer = camera.frame().unwrap();
+    let decoded_image = frame_buffer.decode_image::<RgbFormat>().unwrap();
 
-    let file = File::create("/tmp/cam");
-    let _result = file.unwrap().write_all(&frame);
+    let mut comp = Compress::new(mozjpeg::ColorSpace::JCS_RGB);
+    comp.start_compress();
+    comp.write_scanlines(&decoded_image);
+    comp.finish_compress();
+    
+    let decoded_vec = comp.data_to_vec().unwrap();
+
+    let mut file = File::create("/tmp/file.jpeg").unwrap();
+    let _result = file.write_all(&decoded_vec);
 }
+
